558fda17417c020d7fb74d41baecb2bef349c49d
niahmad7 <niahmad8@gmail.com>
2026-03-04 10:51:01 +0500

    ✨ Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-def main():
-    print("Hello from langchain-chatbot!")
+from langchain_ollama import ChatOllama
 
+llm = ChatOllama(
+    model="minimax-m2.5:cloud",
+    temperature=0.7,
+    # other params...
+)
 
-if __name__ == "__main__":
-    main()
+response = llm.invoke("what is RAG?")
+print(response.content)
