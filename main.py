ca68ea3ff166701ac5edd131096ff75c9a93cf07
niahmad7 <niahmad8@gmail.com>
2026-03-05 07:32:40 +0500

    ✨ Added new logic for LangChain pipeline

--- main.py
+++ main.py
@@ -1,10 +1,31 @@
 from langchain_ollama import ChatOllama
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
+from langchain_core.prompts import ChatPromptTemplate 
+from langchain_core.output_parsers import StrOutputParser
+
+
 
 llm = ChatOllama(
     model="minimax-m2.5:cloud",
     temperature=0.7,
+    num_ctx=2048,
+    num_predict=1024,
     # other params...
 )
 
-response = llm.invoke("what is RAG?")
-print(response.content)
+
+prompt = ChatPromptTemplate.from_messages([
+("system", "You are an expert in, give concise answers."),
+("human", "{question}")
+])
+
+chain = prompt | llm | StrOutputParser() # Pipeline or LCEL for structured output (used pipe operator to chain the prompt, llm and output parser together)
+
+# response = chain.invoke({
+#     "topic": "AI and Machine Learning",
+#     "question": "What is RAG?"
+# })
+# print(response)
+
+for chunks in chain.stream({"question": "What is LangChain?"}):
+     print(chunks, end="", flush=True)
